a1ebaea0180db594f7fbf05394b2aa57237a0c55
ChungKein4320 <trungkien7818@gmail.com>
2024-12-26 20:59:17 +0700

    test

--- backup/assets/js/script.js
+++
@@ -1,9 +0,0 @@
-function showPhoneNumber() {
-    const phoneNumberDiv = document.getElementById("phone-number");
-    if (phoneNumberDiv.style.display === "none" || phoneNumberDiv.style.display === "") {
-        phoneNumberDiv.style.display = "block";
-        phoneNumberDiv.textContent = "Phone Number: 0358316427";
-    } else {
-        phoneNumberDiv.style.display = "none";
-    }
-}
